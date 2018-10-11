af7e1373b495a7ebb55354f69244c59b277c2dc9
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-11 20:00:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,12 @@ class Song
 
   def initialize(name, artist, genre)
     @name = name
+
     @artist = artist
+    @@artists << artist unless @@artists.include?(artist)
+
     @genre = genre
+  @@genres << genre unless @@genres.include?(genre)
     @@count += 1
   end
 
